366675760d56a03f752facd4d1f3e1ce932a809f
Chitsanupong Paenyoi <37466531+JcsnP@users.noreply.github.com>
2020-10-25 00:49:07 +0700

    add file calculate.cpp

---
+++ calculate.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+using namespace std;
+
+int calculate(int a, int b);
+
+int main(){
+    int a, b;
+
+    cout << "first number: " << endl;
+    cin >> a;
+    cout << "second number: " << endl;
+    cin >> b;
+
+    int result = calculate(a, b);
+
+    cout << "Result is: " << result;
+}
+
+int calculate(int a, int b){
+    cout << "Select Operator" << endl;
+    cout << "A) Addition (+)" << endl;
+    cout << "B) Subtraction (-)" << endl;
+    cout << "C) Multiplication (*)" << endl;
+    cout << "D) Division (/)" << endl;
+
+    char operatorSelection;
+    int result = 0;
+
+    cin >> operatorSelection;
+
+    switch (operatorSelection)
+    {
+    case 'A':
+        result = a + b;
+        break;
+    case 'B':
+        if(a > b){
+            result = a - b;
+        } else {
+            result = b -a;
+        }
+        break;
+    case 'C':
+        result = a * b;
+        break;
+    case 'D':
+        if(a > b){
+            result = a / b;
+        } else {
+            result = b / a;
+        }
+        break;
+
+    default:
+        cout << "Select again!!!" << endl;
+        break;
+    }
+
+    return result;
+}
