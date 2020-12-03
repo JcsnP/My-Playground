49314282fbf557f7baf43b3f881cb0bf102ae68a
Chitsanupong Paenyoi <37466531+JcsnP@users.noreply.github.com>
2020-12-03 19:23:33 +0700

    Update matrix001.cpp

--- matrix001.cpp
+++ matrix001.cpp
@@ -1,48 +1,44 @@
-#include <stdio.h>
+#include <bits/stdc++.h>
 
-int main(){
-	int row, column;
-	int matrix1[100][100], matrix2[100][100];
-	
-	//row
-	printf("Input row: ");
-	scanf("%d", &row);
-	
-	//column
-	printf("Input column: ");
-	scanf("%d", &column);
-	
-	//Matrix 1
-	for(int i = 0; i < row; ++i){
-		for(int j = 0;j < column; ++j){
-			printf("Matrix 1 [%d][%d]: ", i+1, j+1);
-			scanf("%d", &matrix1[i][j]);
-		}
-	}
-	
-	//Matrix 2
-	for(int i = 0; i < row; ++i){
-		for(int j = 0;j < column; ++j){
-			printf("Matrix 2 [%d][%d]: ", i+1, j+1);
-			scanf("%d", &matrix2[i][j]);
-		}
-	}
-	
-	//Show Addition Matrix
-	printf("Matrix addition\n");
-	for(int i = 0; i < row; ++i){
-		for(int j = 0;j < column; ++j){
-			printf("%d\t", matrix1[i][j] + matrix2[i][j]);
-		}
-		printf("\n");
-	}
-	
-	//Show Matrix Subtraction
-	printf("Matrix subtraction\n");
-	for(int i = 0; i < row; ++i){
-		for(int j = 0;j < column; ++j){
-			printf("%d\t", matrix1[i][j] - matrix2[i][j]);
-		}
-		printf("\n");
-	}
+int main() {
+    int row, col;
+    int m1[100][100], m2[100][100];
+    
+    std::cout << "Please input row (m): ";
+    std::cin >> row;
+    
+    std::cout << "Please input column (n): ";
+    std::cin >> col;
+    
+    for(int i = 0; i < row; i++){
+        for(int j = 0; j < col; j++){
+            std::cout << "Matrix 1 [" << i+1 << "][" << j+1 << "]: ";
+            std::cin >> m1[i][j];
+        }
+    }
+    
+    for(int i = 0; i < row; i++){
+        for(int j = 0; j < col; j++){
+            std::cout << "Matrix 2 [" << i+1 << "][" << j+1 << "]: ";
+            std::cin >> m2[i][j];
+        }
+    }
+    
+    std::cout << "Matrix Addition" << std::endl;
+    for(int i = 0; i < row; i++){
+        for(int j = 0; j < col; j++){
+            std::cout << m1[i][j] + m2[i][j] << "\t";
+        }
+        std::cout << std::endl;
+    }
+    
+    std::cout << std::endl;
+    
+    std::cout << "Matrix Subtraction" << std::endl;
+    for(int i = 0; i < row; i++){
+        for(int j = 0; j < col; j++){
+            std::cout << m1[i][j] - m2[i][j] << "\t";
+        }
+        std::cout << std::endl;
+    }
 }
