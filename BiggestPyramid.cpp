1412d0f67a710bb5595a878a2b7c671ad9157b53
Chitsanupong Paenyoi <37466531+JcsnP@users.noreply.github.com>
2020-12-09 20:11:56 +0700

    Update BiggestPyramid.cpp

--- BiggestPyramid.cpp
+++ BiggestPyramid.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 
-int main() {
-  int row = 3;
+void pyramid(int row){
   for(int i = 0, f = 1; i < row; i++, f += 2){
     for(int j = row - 1; j > i; j--){
       std::cout << " ";
@@ -11,6 +10,15 @@ int main() {
     }
     std::cout << std::endl;
   } 
+}
+
+int main() {
+  int row;
+  std::cout << "Please input row: ";
+  std::cin >> row;
+
+  //call function
+  pyramid(row);
 }
 
 /*
