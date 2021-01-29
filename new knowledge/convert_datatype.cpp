e03a45fa5f5d8b2e82939c4c279e32ca565de854
Chitsanupong Paenyoi <37466531+JcsnP@users.noreply.github.com>
2021-01-29 15:34:47 +0700

    Create convert_datatype.cpp
    it's change data in variable, not change data type of variable

---
+++ new knowledge/convert_datatype.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	int num = 10;
+	float gpa = 3.90;
+	char A = 'A';
+	
+	cout << "float(num): " << float(num) << endl;
+	cout << "num: " << num << endl;
+	cout << "int(gpa): " << int(gpa) << endl;
+	cout << "gpa: " << gpa << endl;
+	cout << "int(A): " << int(A) << endl;
+	cout << "A: " << A << endl;
+}
+
+/* OUTPUT
+* float(num): 10
+* num: 10
+* int(gpa): 3
+* gpa: 3.9
+* int(A): 65
+* A: A
+*/
